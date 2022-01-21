b65a35ff4321749257a03a903cd6b0cd69b33cb8
Yolanda Ihsan <yolanda.ihsan@8investment.com>
2022-01-21 15:01:39 +0700

    can add youtube

---
+++ site/content/blog/new-test-post.md
@@ -0,0 +1,14 @@
+---
+title: New Test Post
+metaTitle: Meta title for new test post
+metaDescription: meta description for new test post
+date: 2022-01-21T06:42:35.905Z
+intro: Hey this is the intro blurb for new test post
+featuredImage:
+  src: img/home-jumbotron.jpg
+  alt: alt text for test post image
+  title: title text for test post image
+---
+this is the body of content
+
+{{< youtube id="wQUH06QRCi4" >}}
--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -4,7 +4,7 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
+    let image = getAsset(entry.getIn(["data", "featuredImage.src"]));
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
@@ -14,7 +14,7 @@ export default class PostPreview extends React.Component {
       </div>
       <div className="cms mw6">
         <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
+        { image && <img src={ image } alt={ entry.getIn(["data", "featuredImage.alt"])} /> }
         { widgetFor("body") }
       </div>
     </div>;
